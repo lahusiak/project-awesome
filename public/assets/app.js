dfb63339872b8843eb1db8fa6b8914dc25344dc6
lahusiak <lahusiak@gmail.com>
2015-10-29 13:07:17 -0500

    Update

--- public/assets/app.js
+++ public/assets/app.js
@@ -3,10 +3,30 @@ var frontendValue = randomNumber(10, 60);
 var backendValue = randomNumber(10, 60);
 var logicValue = randomNumber(10, 60);
 
+var frontendScrumPoints = 0;
+var backendScrumPoints = 0;
+var logicScrumPoints = 0;
+
+var frontendEmployeeCount = 0;
+var backendEmployeeCount = 0;
+var logicEmployeeCount = 0;
+
+var totalWeeks = 0;
+
+var employeeArray = [employee1, employee2, employee3];
+
+
 $(document).ready(function(){
     $(".generate").on("click", function(){
         appendDom();
-        console.log(frontendValue);
+        //console.log(frontendValue);
+        addScrumAndSkillPoints(employee1);
+        console.log("This is employee1:" , employee1);
+        addScrumAndSkillPoints(employee2);
+        addScrumAndSkillPoints(employee3);
+        //console.log("This is Employee Count: " + backendEmployeeCount);
+        //console.log("This is Scrum points: " + backendScrumPoints);
+
     });
 
 });
@@ -25,7 +45,97 @@ function randomNumber(min, max){
 
 
 
-//function randomFrontend(){
-//    frontendValue = randomNumber(10, 60);
-//    return frontendValue;
+function metCriteria() {
+    if (frontendEmployeeCount > 0
+        && backendEmployeeCount > 0
+        && logicEmployeeCount > 0) {
+        console.log ("Criteria met");
+        findTotalWeeks();
+
+    }else{
+        console.log("Criteria not met yet");
+    }
+};
+
+
+var employee1 = {
+    name:"Joe",
+    skill:"backEnd",
+    scrum:5
+};
+
+var employee2 = {
+    name:"Mary",
+    skill:"frontEnd",
+    scrum:3
+};
+
+var employee3 = {
+    name:"Jan",
+    skill:"logic",
+    scrum:7
+};
+
+function addScrumAndSkillPoints(employee){
+    console.log("This is employee: ", employee);
+    if(employee.skill == "frontEnd") {
+        frontendEmployeeCount++;
+        frontendScrumPoints += employee.scrum;
+    }else if(employee.skill == "backEnd"){
+        backendEmployeeCount++;
+        backendScrumPoints += employee.scrum;
+        console.log("This is the backEnd employee");
+    }else if(employee.skill == "logic"){
+        logicEmployeeCount++;
+        logicScrumPoints += employee.scrum;
+    }else{
+        console.log("Employee not found");
+    }
+    metCriteria();
+}
+
+function findTotalWeeks(){
+   var tempFront = frontendValue/frontendScrumPoints;
+    var tempBack = backendValue/backendScrumPoints;
+     var tempLogic = logicValue/logicScrumPoints;
+
+   totalWeeks = Math.max(tempFront, tempBack, tempLogic);
+
+    console.log("this is total weeks: ", totalWeeks);
+
+}
+
+function addEmployee(){
+
+}
+
+//
+//function addScrumAndSkillPoints(buttonClicked){
+//
+//    switch(buttonClicked) {
+//        //check against employee module object incoming from server
+//        case "frontEnd":
+//            frontendEmployeeCount++;
+//            frontendScrumPoints += frontendScrumPoints;
+//            break;
+//        case "backEnd":
+//            backendEmployeeCount++;
+//            backendScrumPoints += backendScrumPoints;
+//            break;
+//        case "logic":
+//            logicEmployeeCount++;
+//            logicScrumPoints += logicScrumPoints;
+//            break;
+//
+//    }
+//}
+
+//function generateEmployeeName(){
+//    //generate a number between 1 and 100 and get name
+//}
+//function generateCompanyName(){
+//    //generate number 1-100
+//}
+//function generateEmployee(){
+//    //generate number 1-100
 //}
